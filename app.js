6755f494c6515c8b0bf86c8b62711b7204223312
Karl Bernardo <stephene.bernardo@gmail.com>
2019-10-19 13:45:47 +0800

    completed crud operation for user management

--- app.js
+++ app.js
@@ -4,26 +4,47 @@ const port = 4201
 const Sequelize = require('sequelize');
 const InitDb = require('./initdb')
 const UserModel = require('./userModel')
+const UserApi = require('./services/userApi')
+const bodyParser     =         require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const sequelize = new Sequelize('postgres', 'postgres', 'mysecretpassword', {
   port: 5433,
   dialect:'postgres'
 });
 const User = sequelize.define('user', UserModel, {});
+const userApi = new UserApi(User);
 InitDb(sequelize).then(() => {
-  app.get('/', async (req, res) => {
-    await User.create({
-      firstName:'Karl',
-      lastName: 'Bernardo',
-      userName:'lrak'
-    }).then(()=>{
-        console.log('asdas')
-    })
+  app.get('/users', async (req, res) => {
+    users = await userApi.findAll();
+    res.send(users)
+  });
 
-    // await User.findAll().then(users => {
-    //   console.log("All users:", JSON.stringify(users, null, 4));
-    // });
-    res.send('Hello World!')
+  app.post('/user', async (req, res)=> {
+    const {firstName, lastName, userName} = req.body;
+    const user = await userApi.insert(firstName, lastName, userName);
+    res.send(`Successfully create user ${user.id}`)
+  });
+
+  app.get('/user/:id', async (req, res) => {
+    const id = req.params.id
+    const user = await userApi.get(id)
+    res.send(user)
+  });
+
+  app.patch('/user/:id', async (req, res)=> {
+    const id = req.params.id
+    const {firstName, lastName, userName} = req.body;
+    const user = await userApi.update(id, {firstName, lastName, userName});
+    res.send(`Successfully updated user ${id}`)
+  });
+
+  app.delete('/user/:id', async (req, res) => {
+    const id = req.params.id
+    await userApi.delete(id)
+    res.send(`Successfuly remove user ${id}`)
   });
 
   app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ services/userApi.js
@@ -0,0 +1,26 @@
+module.exports = class UserApi {
+  constructor(userDb){
+    this.userDb = userDb;
+  }
+
+  findAll (){
+    return this.userDb.findAll();
+  }
+
+  insert(firstName, lastName, userName) {
+    return this.userDb.create({firstName, lastName, userName})
+  }
+
+  update(id, newValues){
+    Object.keys(newValues).forEach(key => newValues[key] === undefined && delete newValues[key])
+    console.log(newValues)
+    return this.userDb.update({ ...newValues }, {where: {id}})
+  }
+
+  get(id){
+    return this.userDb.findAll({where: {id}})
+  }
+  delete(id){
+    return this.userDb.destroy({where: {id}})
+  }
+};
